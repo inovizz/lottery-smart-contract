a411a0d5995b27d1494a5598eec2c2f951878d81
Sanchit Balchandani <balchandani.sanchit@gmail.com>
2018-02-06 21:09:07 +0530

    Add docstrings in Lottery Smart Contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,9 +1,22 @@
+/**
+  * @Lottery.sol
+  * @author  Sanchit Balchandani <balchandani.sanchit@gmail.com>
+  *
+  * The smart contract is about a Lottery Game on ethereum blockchain
+  * which allows owner of the contract to instantiate the lottery and have
+  * the first ticket on his name, as soon as all tickets are sold out, a
+  * winner is declared via Event and the winningAmount is transferred to
+  * the winner's address.
+  * Incase, all tickets doesn't get sold, so owner has the functionality to
+  * end the lottery, which would find the winner at that particular point and
+  * send the winningAmount to winner's address.
+ */
 pragma solidity 0.4.18;
 
 import "./helper_contracts/zeppelin/Ownable.sol";
 
 
-/** @title Ethereum Lottery Smart Contract. */
+/** Ethereum Lottery Smart Contract.*/
 contract Lottery is Ownable {
     uint internal numTickets;
     uint internal availTickets;
@@ -12,26 +25,29 @@ contract Lottery is Ownable {
     bool internal gameStatus;
     uint internal counter;
 
+    // mapping to have counter to address value.
     mapping (uint => address) internal players;
+    // mapping to have address to bool value.
     mapping (address => bool) internal playerAddresses;
 
-    event Winner(uint indexed counter, address winner, string mesg); 
+    // Event which would be emmitted once winner is found.
+    event Winner(uint indexed counter, address winner, string mesg);
 
-    /** @dev returns the Lotter status.
+    /** getLotteryStatus function returns the Lotter status.
       * @return numTickets The total # of lottery tickets.
       * @return availTickets The # of available tickets.
       * @return ticketPrice The price for one lottery ticket.
       * @return gameStatus The Status of lottery game.
       * @return contractBalance The total available balance of the contract.
-    */
+     */
     function getLotteryStatus() public view returns(uint, uint, uint, bool, uint) {
         return (numTickets, availTickets, ticketPrice, gameStatus, winningAmount);
     }
 
-    /** @dev inititates the lottery game with #tickets and ticket price.
+    /** startLottery function inititates the lottery game with #tickets and ticket price.
       * @param tickets - no of max tickets.
       * @param price - price of the ticket.
-    */
+     */
     function startLottery(uint tickets, uint price) public payable onlyOwner {
         if (tickets <= 1) {
             revert();
@@ -46,19 +62,26 @@ contract Lottery is Ownable {
         ticketPrice = price;
         availTickets = numTickets - 1;
         players[++counter] = owner;
+        // increase the winningAmount
         winningAmount += msg.value;
+        // set the gameStatus to True
         gameStatus = true;
         playerAddresses[owner] = true;
     }
 
-    /** @dev play lottery game. */
+    /** function playLotter allows user to buy tickets and finds the winnner,
+      * when all tickets are sold out.
+     */
     function playLottery() public payable {
+        // revert in case user already has bought a ticket.
         if (playerAddresses[msg.sender]) {
             revert();
         }
+        // revert incase value sent is less than the ticket price.
         if (msg.value < ticketPrice) {
             revert();
         }
+        // revert in case gameStatus is false
         if (!gameStatus) {
             revert();
         }
@@ -66,28 +89,29 @@ contract Lottery is Ownable {
         players[++counter] = msg.sender;
         winningAmount += msg.value;
         playerAddresses[msg.sender] = true;
+        // reset the Lotter as soon as availTickets are zero.
         if (availTickets == 0) {
             resetLottery();
         }
     }
 
-    /** @dev getter function for gameStatus.
-      * @return gameStatus - current status of lottery game.
-    */
+    /** getGameStatus function to get value of gameStatus.
+      * @return gameStatus - current status of the lottery game.
+     */
     function getGameStatus() public view returns(bool) {
         return gameStatus;
     }
 
-    /** @dev endLottery function.
-    */
+    /** endLottery function which would be called only by Owner.
+     */
     function endLottery() public onlyOwner {
         resetLottery();
     }
-    
-    /** @dev getWinner function.
+
+    /** getWinner getter function.
       * this calls getRandomNumber function and
       * finds the winner using players mapping
-    */
+     */
     function getWinner() internal {
         uint winnerIndex = getRandomNumber();
         address winnerAddress = players[winnerIndex];
@@ -95,15 +119,15 @@ contract Lottery is Ownable {
         winnerAddress.transfer(winningAmount);
     }
 
-    /** @dev resetLotter function.
-    */
+    /** getRandomNumber function, which finds the random number using counter.
+     */
     function getRandomNumber() internal view returns(uint) {
         uint random = uint(block.blockhash(block.number-1))%counter + 1;
         return random;
     }
 
-    /** @dev resetLotter function.
-    */
+    /** resetLottery function resets lottery and find the Winner.
+     */
     function resetLottery() internal {
         gameStatus = false;
         getWinner();
@@ -113,5 +137,4 @@ contract Lottery is Ownable {
         ticketPrice = 0;
         counter = 0;
     }
-
 }
